71b75d6dbf87231c128cd954560627cb9490de08
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-10 23:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ class Tree {
   }
   
   static definition() {
-    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
   }
   
-  
+  class Deciduous extends Tree {
+    constructor(species, name) {
+      this.species = species; 
+      this.name = name; 
+    }
+  }
 }
