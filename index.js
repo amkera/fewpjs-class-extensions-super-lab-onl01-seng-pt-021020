72f3fb83be9fb03f93c0c628a1b3d39e7ad4dff4
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-10 23:54:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your classes here
+
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+  
+  static definition() {
+    return "Trees are very large plants that provide us with oxygen."
+  }
+  
+  
+}
